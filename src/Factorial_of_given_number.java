354e5aeea61e84cb7ac820f881edf8b7577225bf
ujjawal kumar <ujjawalfrds@gmail.com>
2023-02-03 17:53:39 +0530

    I have made some logical changes in my code

---
+++ src/Factorial_of_given_number.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Factorial_of_given_number {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        long a=sc.nextLong();
+        long i=1;
+        long fact=1;
+        while(i<=a){
+            fact=fact*i;
+            i++;
+
+
+        }
+        System.out.println("The factorial of given number is:"+fact);
+    }
+}
